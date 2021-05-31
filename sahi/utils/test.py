dd338c7d573336a83348b880ab939554e4005ab1
fcakyon <fcakyon@gmail.com>
2021-05-31 22:01:22 +0300

    update test utils

--- sahi/utils/test.py
+++ sahi/utils/test.py
@@ -1,7 +1,7 @@
 import urllib.request
 from os import path
-
-from sahi.utils.file import create_dir
+from pathlib import Path
+from typing import Optional
 
 
 def mmdet_version_as_integer():
@@ -12,57 +12,69 @@ def mmdet_version_as_integer():
 
 class MmdetTestConstants:
     MMDET_CASCADEMASKRCNN_MODEL_URL = "http://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_mask_rcnn_r50_fpn_1x_coco/cascade_mask_rcnn_r50_fpn_1x_coco_20200203-9d4dcb24.pth"
-    MMDET_CASCADEMASKRCNN_MODEL_PATH = "tests/data/models/mmdet_cascade_mask_rcnn/cascade_mask_rcnn_r50_fpn_1x_coco_20200203-9d4dcb24.pth"
+    MMDET_CASCADEMASKRCNN_MODEL_PATH = (
+        "tests/data/models/mmdet_cascade_mask_rcnn/cascade_mask_rcnn_r50_fpn_1x_coco_20200203-9d4dcb24.pth"
+    )
     MMDET_RETINANET_MODEL_URL = "http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r50_fpn_2x_coco/retinanet_r50_fpn_2x_coco_20200131-fdb43119.pth"
     MMDET_RETINANET_MODEL_PATH = "tests/data/models/mmdet_retinanet/retinanet_r50_fpn_2x_coco_20200131-fdb43119.pth"
 
     if mmdet_version_as_integer() < 290:
-        MMDET_CASCADEMASKRCNN_CONFIG_PATH = "tests/data/models/mmdet_cascade_mask_rcnn/cascade_mask_rcnn_r50_fpn_1x_coco_v280.py"
-        MMDET_RETINANET_CONFIG_PATH = (
-            "tests/data/models/mmdet_retinanet/retinanet_r50_fpn_1x_coco_v280.py"
+        MMDET_CASCADEMASKRCNN_CONFIG_PATH = (
+            "tests/data/models/mmdet_cascade_mask_rcnn/cascade_mask_rcnn_r50_fpn_1x_coco_v280.py"
         )
+        MMDET_RETINANET_CONFIG_PATH = "tests/data/models/mmdet_retinanet/retinanet_r50_fpn_1x_coco_v280.py"
     else:
-        MMDET_CASCADEMASKRCNN_CONFIG_PATH = "tests/data/models/mmdet_cascade_mask_rcnn/cascade_mask_rcnn_r50_fpn_1x_coco.py"
-        MMDET_RETINANET_CONFIG_PATH = (
-            "tests/data/models/mmdet_retinanet/retinanet_r50_fpn_1x_coco.py"
+        MMDET_CASCADEMASKRCNN_CONFIG_PATH = (
+            "tests/data/models/mmdet_cascade_mask_rcnn/cascade_mask_rcnn_r50_fpn_1x_coco.py"
         )
+        MMDET_RETINANET_CONFIG_PATH = "tests/data/models/mmdet_retinanet/retinanet_r50_fpn_1x_coco.py"
 
 
 class Yolov5TestConstants:
-    YOLOV5S6_MODEL_URL = (
-        "https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5s6.pt"
-    )
+    YOLOV5S6_MODEL_URL = "https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5s6.pt"
     YOLOV5S6_MODEL_PATH = "tests/data/models/yolov5/yolov5s6.pt"
 
+    YOLOV5M6_MODEL_URL = "https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5m6.pt"
+    YOLOV5M6_MODEL_PATH = "tests/data/models/yolov5/yolov5m6.pt"
 
-def download_mmdet_cascade_mask_rcnn_model():
 
-    create_dir("tests/data/models/mmdet_cascade_mask_rcnn/")
+def download_mmdet_cascade_mask_rcnn_model(destination_path: Optional[str] = None):
 
-    if not path.exists(MmdetTestConstants.MMDET_CASCADEMASKRCNN_MODEL_PATH):
+    if destination_path is None:
+        destination_path = MmdetTestConstants.MMDET_CASCADEMASKRCNN_MODEL_PATH
+
+    Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
+
+    if not path.exists(destination_path):
         urllib.request.urlretrieve(
             MmdetTestConstants.MMDET_CASCADEMASKRCNN_MODEL_URL,
-            MmdetTestConstants.MMDET_CASCADEMASKRCNN_MODEL_PATH,
+            destination_path,
         )
 
 
-def download_mmdet_retinanet_model():
+def download_mmdet_retinanet_model(destination_path: Optional[str] = None):
 
-    create_dir("tests/data/models/mmdet_retinanet/")
+    if destination_path is None:
+        destination_path = MmdetTestConstants.MMDET_RETINANET_MODEL_PATH
 
-    if not path.exists(MmdetTestConstants.MMDET_RETINANET_MODEL_PATH):
+    Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
+
+    if not path.exists(destination_path):
         urllib.request.urlretrieve(
             MmdetTestConstants.MMDET_RETINANET_MODEL_URL,
-            MmdetTestConstants.MMDET_RETINANET_MODEL_PATH,
+            destination_path,
         )
 
 
-def download_yolov5s6_model():
+def download_yolov5s6_model(destination_path: Optional[str] = None):
 
-    create_dir("tests/data/models/yolov5/")
+    if destination_path is None:
+        destination_path = Yolov5TestConstants.YOLOV5S6_MODEL_PATH
 
-    if not path.exists(Yolov5TestConstants.YOLOV5S6_MODEL_PATH):
+    Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
+
+    if not path.exists(destination_path):
         urllib.request.urlretrieve(
             Yolov5TestConstants.YOLOV5S6_MODEL_URL,
-            Yolov5TestConstants.YOLOV5S6_MODEL_PATH,
+            destination_path,
         )
