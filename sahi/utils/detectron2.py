d8dc4357ad466d27285b472181069dd13ddd67a2
fcakyon <34196005+fcakyon@users.noreply.github.com>
2022-01-01 16:56:53 +0300

    fix detectron class

--- sahi/utils/detectron2.py
+++ sahi/utils/detectron2.py
@@ -1,2 +1,4 @@
 class Detectron2TestConstants:
-    FASTERCNN_MODEL_ZOO_NAME = "COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml"
+    FASTERCNN_MODEL_ZOO_NAME = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+    RETINANET_MODEL_ZOO_NAME = "COCO-Detection/retinanet_R_50_FPN_3x.yaml"
+    MASKRCNN_MODEL_ZOO_NAME = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
