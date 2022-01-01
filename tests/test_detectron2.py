8bc344e7a0df903b1c48a8b8245e5228ca35275a
fcakyon <34196005+fcakyon@users.noreply.github.com>
2022-01-01 17:09:25 +0300

    add margin to bbox tests

--- tests/test_detectron2.py
+++ tests/test_detectron2.py
@@ -82,16 +82,19 @@ class TestDetectron2DetectionModel(unittest.TestCase):
         self.assertEqual(len(object_prediction_list), 10)
         self.assertEqual(object_prediction_list[0].category.id, 2)
         self.assertEqual(object_prediction_list[0].category.name, "car")
-        self.assertEqual(
-            object_prediction_list[0].bbox.to_coco_bbox(),
-            [320, 324, 61, 40],
-        )
+        predicted_bbox = object_prediction_list[0].bbox.to_coco_bbox()
+        desired_bbox = [320, 324, 61, 40]
+        margin = 4
+        for ind, point in enumerate(predicted_bbox):
+            assert point < desired_bbox[ind] + margin and point > desired_bbox[ind] - margin
+
         self.assertEqual(object_prediction_list[5].category.id, 2)
         self.assertEqual(object_prediction_list[5].category.name, "car")
-        self.assertEqual(
-            object_prediction_list[2].bbox.to_coco_bbox(),
-            [383, 275, 35, 33],
-        )
+        predicted_bbox = object_prediction_list[2].bbox.to_coco_bbox()
+        desired_bbox = [383, 275, 35, 33]
+        margin = 4
+        for ind, point in enumerate(predicted_bbox):
+            assert point < desired_bbox[ind] + margin and point > desired_bbox[ind] - margin
 
     def test_convert_original_predictions_with_mask_output(self):
 
@@ -120,16 +123,19 @@ class TestDetectron2DetectionModel(unittest.TestCase):
         self.assertEqual(len(object_prediction_list), 10)
         self.assertEqual(object_prediction_list[0].category.id, 2)
         self.assertEqual(object_prediction_list[0].category.name, "car")
-        self.assertEqual(
-            object_prediction_list[0].bbox.to_coco_bbox(),
-            [322, 325, 57, 38],
-        )
+        predicted_bbox = object_prediction_list[0].bbox.to_coco_bbox()
+        desired_bbox = [322, 325, 57, 38]
+        margin = 4
+        for ind, point in enumerate(predicted_bbox):
+            assert point < desired_bbox[ind] + margin and point > desired_bbox[ind] - margin
+
         self.assertEqual(object_prediction_list[5].category.id, 2)
         self.assertEqual(object_prediction_list[5].category.name, "car")
-        self.assertEqual(
-            object_prediction_list[5].bbox.to_coco_bbox(),
-            [697, 226, 29, 29],
-        )
+        predicted_bbox = object_prediction_list[5].bbox.to_coco_bbox()
+        desired_bbox = [697, 226, 29, 29]
+        margin = 4
+        for ind, point in enumerate(predicted_bbox):
+            assert point < desired_bbox[ind] + margin and point > desired_bbox[ind] - margin
 
 
 if __name__ == "__main__":
