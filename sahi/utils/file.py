e504fa9bee6da049001997aa897751304d4f8d0b
fatih <34196005+fcakyon@users.noreply.github.com>
2022-05-22 09:24:57 +0300

    fix linting

--- sahi/utils/file.py
+++ sahi/utils/file.py
@@ -55,7 +55,7 @@ class NumpyEncoder(json.JSONEncoder):
             return super(NumpyEncoder, self).default(obj)
 
 
-def load_json(load_path:str):
+def load_json(load_path: str):
     encoding = "utf-8"
     """
     Loads json formatted data (given as "data") from load_path
