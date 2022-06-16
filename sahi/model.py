7c5113292b1f4363380d3d241d2501f068fdce62
fcakyon <fcakyon@gmail.com>
2022-06-16 22:55:53 +0300

    add torchvision detector support

--- sahi/model.py
+++ sahi/model.py
@@ -703,3 +703,209 @@ class Detectron2DetectionModel(DetectionModel):
         object_prediction_list_per_image = [object_prediction_list]
 
         self._object_prediction_list_per_image = object_prediction_list_per_image
+
+
+@check_requirements(["torchvision", "torch"])
+class TorchVisionDetectionModel(DetectionModel):
+    def __init__(
+        self,
+        model_path: Optional[str] = None,
+        model: Optional[Any] = None,
+        config_path: Optional[str] = None,
+        device: Optional[str] = None,
+        mask_threshold: float = 0.5,
+        confidence_threshold: float = 0.3,
+        category_mapping: Optional[Dict] = None,
+        category_remapping: Optional[Dict] = None,
+        load_at_init: bool = True,
+        image_size: int = None,
+    ):
+
+        super().__init__(
+            model_path=model_path,
+            model=model,
+            config_path=config_path,
+            device=device,
+            mask_threshold=mask_threshold,
+            confidence_threshold=confidence_threshold,
+            category_mapping=category_mapping,
+            category_remapping=category_remapping,
+            load_at_init=load_at_init,
+            image_size=image_size,
+        )
+
+    def load_model(self):
+        import torch
+
+        from sahi.utils.torchvision import MODEL_NAME_TO_CONSTRUCTOR
+
+        # read config params
+        model_name = None
+        num_classes = None
+        if self.config_path is not None:
+            import yaml
+
+            with open(self.config_path, "r") as stream:
+                try:
+                    config = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    raise RuntimeError(exc)
+
+            model_name = config.get("model_name", None)
+            num_classes = config.get("num_classes", None)
+
+        # complete params if not provided in config
+        if not model_name:
+            model_name = "fasterrcnn_resnet50_fpn"
+            logger.warning(f"model_name not provided in config, using default model_type: {model_name}'")
+        if num_classes is None:
+            logger.warning("num_classes not provided in config, using default num_classes: 91")
+            num_classes = 91
+        if self.model_path is None:
+            logger.warning("model_path not provided in config, using pretrained weights and default num_classes: 91.")
+            pretrained = True
+            num_classes = 91
+        else:
+            pretrained = False
+
+        # load model
+        model = MODEL_NAME_TO_CONSTRUCTOR[model_name](num_classes=num_classes, pretrained=pretrained)
+        try:
+            model.load_state_dict(torch.load(self.model_path))
+        except Exception as e:
+            TypeError("model_path is not a valid torchvision model path: ", e)
+
+        self.set_model(model)
+
+    def set_model(self, model: Any):
+        """
+        Sets the underlying TorchVision model.
+        Args:
+            model: Any
+                A TorchVision model
+        """
+
+        model.eval()
+        self.model = model.to(self.device)
+
+        # set category_mapping
+        from sahi.utils.torchvision import COCO_CLASSES
+
+        if self.category_mapping is None:
+            category_names = {str(i): COCO_CLASSES[i] for i in range(len(COCO_CLASSES))}
+            self.category_mapping = category_names
+
+    def perform_inference(self, image: np.ndarray, image_size: int = None):
+        """
+        Prediction is performed using self.model and the prediction result is set to self._original_predictions.
+        Args:
+            image: np.ndarray
+                A numpy array that contains the image to be predicted. 3 channel image should be in RGB order.
+            image_size: int
+                Inference input size.
+        """
+        from sahi.utils.torch import to_float_tensor
+
+        # arrange model input size
+        if self.image_size is not None:
+            # get min and max of image height and width
+            min_shape, max_shape = min(image.shape[:2]), max(image.shape[:2])
+            # torchvision resize transform scales the shorter dimension to the target size
+            # we want to scale the longer dimension to the target size
+            image_size = self.image_size * min_shape / max_shape
+            self.model.transform.min_size = (image_size,)  # default is (800,)
+            self.model.transform.max_size = image_size  # default is 1333
+
+        image = to_float_tensor(image)
+        image = image.to(self.device)
+        prediction_result = self.model([image])
+
+        self._original_predictions = prediction_result
+
+    @property
+    def num_categories(self):
+        """
+        Returns number of categories
+        """
+        return len(self.category_mapping)
+
+    @property
+    def has_mask(self):
+        """
+        Returns if model output contains segmentation mask
+        """
+        return self.model.with_mask
+
+    @property
+    def category_names(self):
+        return list(self.category_mapping.values())
+
+    def _create_object_prediction_list_from_original_predictions(
+        self,
+        shift_amount_list: Optional[List[List[int]]] = [[0, 0]],
+        full_shape_list: Optional[List[List[int]]] = None,
+    ):
+        """
+        self._original_predictions is converted to a list of prediction.ObjectPrediction and set to
+        self._object_prediction_list_per_image.
+        Args:
+            shift_amount_list: list of list
+                To shift the box and mask predictions from sliced image to full sized image, should
+                be in the form of List[[shift_x, shift_y],[shift_x, shift_y],...]
+            full_shape_list: list of list
+                Size of the full image after shifting, should be in the form of
+                List[[height, width],[height, width],...]
+        """
+        original_predictions = self._original_predictions
+
+        # compatilibty for sahi v0.8.20
+        if isinstance(shift_amount_list[0], int):
+            shift_amount_list = [shift_amount_list]
+        if full_shape_list is not None and isinstance(full_shape_list[0], int):
+            full_shape_list = [full_shape_list]
+
+        for image_predictions in original_predictions:
+            object_prediction_list_per_image = []
+
+            # get indices of boxes with score > confidence_threshold
+            scores = image_predictions["scores"].cpu().detach().numpy()
+            selected_indices = np.where(scores > self.confidence_threshold)[0]
+
+            # parse boxes, masks, scores, category_ids from predictions
+            category_ids = list(image_predictions["labels"][selected_indices].cpu().detach().numpy())
+            boxes = list(image_predictions["boxes"][selected_indices].cpu().detach().numpy())
+            scores = scores[selected_indices]
+
+            # check if predictions contain mask
+            masks = image_predictions.get("masks", None)
+            if masks is not None:
+                masks = list(image_predictions["masks"][selected_indices].cpu().detach().numpy())
+            else:
+                masks = None
+
+            # create object_prediction_list
+            object_prediction_list = []
+
+            shift_amount = shift_amount_list[0]
+            full_shape = None if full_shape_list is None else full_shape_list[0]
+
+            for ind in range(len(boxes)):
+
+                if masks is not None:
+                    mask = np.array(masks[ind])
+                else:
+                    mask = None
+
+                object_prediction = ObjectPrediction(
+                    bbox=boxes[ind],
+                    bool_mask=mask,
+                    category_id=int(category_ids[ind]),
+                    category_name=self.category_mapping[str(int(category_ids[ind]))],
+                    shift_amount=shift_amount,
+                    score=scores[ind],
+                    full_shape=full_shape,
+                )
+                object_prediction_list.append(object_prediction)
+            object_prediction_list_per_image.append(object_prediction_list)
+
+        self._object_prediction_list_per_image = object_prediction_list_per_image
---
+++ tests/test_torchvision.py
@@ -0,0 +1,256 @@
+# OBSS SAHI Tool
+# Code written by Kadir Nar, 2022.
+
+
+import unittest
+
+from sahi.utils.cv import read_image
+from sahi.utils.torchvision import TorchVisionTestConstants
+
+MODEL_DEVICE = "cpu"
+CONFIDENCE_THRESHOLD = 0.5
+IMAGE_SIZE = 320
+
+
+class TestTorchVisionDetectionModel(unittest.TestCase):
+    def test_load_model(self):
+        from torchvision.models.detection.faster_rcnn import RoIHeads
+
+        from sahi.model import TorchVisionDetectionModel
+
+        torchvision_detection_model = TorchVisionDetectionModel(
+            config_path=TorchVisionTestConstants.FASTERRCNN_CONFIG_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+        )
+        self.assertEqual(isinstance(torchvision_detection_model.model.roi_heads, RoIHeads), True)
+
+    def test_load_model_without_config_path(self):
+        from torchvision.models.detection.faster_rcnn import RoIHeads
+
+        from sahi.model import TorchVisionDetectionModel
+
+        torchvision_detection_model = TorchVisionDetectionModel(
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+        )
+        self.assertEqual(isinstance(torchvision_detection_model.model.roi_heads, RoIHeads), True)
+
+    def test_set_model(self):
+        import torchvision
+        from torchvision.models.detection.ssd import SSDHead
+
+        from sahi.model import TorchVisionDetectionModel
+
+        NUM_CLASSES = 15
+        PRETRAINED = False
+
+        model = torchvision.models.detection.ssd300_vgg16(num_classes=NUM_CLASSES, pretrained=PRETRAINED)
+        torchvision_detection_model = TorchVisionDetectionModel(
+            model=model,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+        )
+
+        self.assertEqual(isinstance(torchvision_detection_model.model.head, SSDHead), True)
+
+    def test_perform_inference_without_mask_output(self):
+        from sahi.model import TorchVisionDetectionModel
+
+        # init model
+        torchvision_detection_model = TorchVisionDetectionModel(
+            config_path=TorchVisionTestConstants.SSD300_CONFIG_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+            image_size=IMAGE_SIZE,
+        )
+        # prepare image
+        image_path = "tests/data/small-vehicles1.jpeg"
+        image = read_image(image_path)
+
+        # perform inference
+        torchvision_detection_model.perform_inference(image)
+        original_predictions = torchvision_detection_model.original_predictions
+
+        from sahi.utils.torchvision import COCO_CLASSES
+
+        boxes = list(original_predictions[0]["boxes"].cpu().detach().numpy())
+        scores = list(original_predictions[0]["scores"].cpu().detach().numpy())
+        category_ids = list(original_predictions[0]["labels"].cpu().detach().numpy())
+
+        # get image height and width
+        image_height, image_width = image.shape[:2]
+
+        # ensure all box coords are valid
+        for box_ind in range(len(boxes)):
+            self.assertEqual(len(boxes[box_ind]), 4)
+            self.assertTrue(boxes[box_ind][0] <= image_width)
+            self.assertTrue(boxes[box_ind][1] <= image_height)
+            self.assertTrue(boxes[box_ind][2] <= image_width)
+            self.assertTrue(boxes[box_ind][3] <= image_height)
+            for coord_ind in range(len(boxes[box_ind])):
+                self.assertTrue(boxes[box_ind][coord_ind] >= 0)
+
+        # ensure all category ids are valid
+        for category_id_ind in range(len(category_ids)):
+            self.assertTrue(category_ids[category_id_ind] < len(COCO_CLASSES))
+            self.assertTrue(category_ids[category_id_ind] >= 0)
+
+        # ensure all scores are valid
+        for score_ind in range(len(scores)):
+            self.assertTrue(scores[score_ind] <= 1)
+            self.assertTrue(scores[score_ind] >= 0)
+
+    def test_convert_original_predictions_without_mask_output(self):
+        from sahi.model import TorchVisionDetectionModel
+
+        torchvision_detection_model = TorchVisionDetectionModel(
+            config_path=TorchVisionTestConstants.FASTERRCNN_CONFIG_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+            image_size=IMAGE_SIZE,
+        )
+
+        # prepare image
+        image_path = "tests/data/small-vehicles1.jpeg"
+        image = read_image(image_path)
+
+        # perform inference
+        torchvision_detection_model.perform_inference(image)
+
+        # convert predictions to ObjectPrediction list
+        torchvision_detection_model.convert_original_predictions()
+        object_prediction_list = torchvision_detection_model.object_prediction_list
+
+        # compare
+        self.assertEqual(len(object_prediction_list), 7)
+        self.assertEqual(object_prediction_list[0].category.id, 3)
+        self.assertEqual(object_prediction_list[0].category.name, "car")
+        self.assertEqual(object_prediction_list[0].bbox.to_coco_bbox(), [315, 309, 65, 57])
+
+    def test_convert_original_predictions_with_mask_output(self):
+        from sahi.model import TorchVisionDetectionModel
+
+        torchvision_detection_model = TorchVisionDetectionModel(
+            config_path=TorchVisionTestConstants.FASTERRCNN_CONFIG_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+            image_size=IMAGE_SIZE,
+        )
+
+        # prepare image
+        image_path = "tests/data/small-vehicles1.jpeg"
+        image = read_image(image_path)
+
+        # perform inference
+        torchvision_detection_model.perform_inference(image)
+
+        # convert predictions to ObjectPrediction list
+        torchvision_detection_model.convert_original_predictions()
+        object_prediction_list = torchvision_detection_model.object_prediction_list
+
+        # compare
+        self.assertEqual(len(object_prediction_list), 7)
+        self.assertEqual(object_prediction_list[0].category.id, 3)
+        self.assertEqual(object_prediction_list[0].category.name, "car")
+        self.assertEqual(object_prediction_list[0].bbox.to_coco_bbox(), [315, 309, 65, 57])
+
+    def test_get_prediction_torchvision(self):
+        from sahi.model import TorchVisionDetectionModel
+        from sahi.predict import get_prediction
+
+        # init model
+        torchvision_detection_model = TorchVisionDetectionModel(
+            config_path=TorchVisionTestConstants.FASTERRCNN_CONFIG_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=False,
+            image_size=IMAGE_SIZE,
+        )
+        torchvision_detection_model.load_model()
+
+        # prepare image
+        image_path = "tests/data/small-vehicles1.jpeg"
+        image = read_image(image_path)
+
+        # get full sized prediction
+        prediction_result = get_prediction(
+            image=image,
+            detection_model=torchvision_detection_model,
+            shift_amount=[0, 0],
+            full_shape=None,
+            postprocess=None,
+        )
+        object_prediction_list = prediction_result.object_prediction_list
+
+        # compare
+        self.assertEqual(len(object_prediction_list), 7)
+        self.assertEqual(object_prediction_list[0].category.id, 3)
+        self.assertEqual(object_prediction_list[0].category.name, "car")
+        self.assertEqual(object_prediction_list[0].bbox.to_coco_bbox(), [315, 309, 65, 57])
+
+    def test_get_sliced_prediction_torchvision(self):
+        from sahi.model import TorchVisionDetectionModel
+        from sahi.predict import get_sliced_prediction
+
+        # init model
+        torchvision_detection_model = TorchVisionDetectionModel(
+            config_path=TorchVisionTestConstants.FASTERRCNN_CONFIG_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=False,
+            image_size=IMAGE_SIZE,
+        )
+        torchvision_detection_model.load_model()
+
+        # prepare image
+        image_path = "tests/data/small-vehicles1.jpeg"
+
+        slice_height = 512
+        slice_width = 512
+        overlap_height_ratio = 0.1
+        overlap_width_ratio = 0.2
+        postprocess_type = "GREEDYNMM"
+        match_metric = "IOS"
+        match_threshold = 0.5
+        class_agnostic = True
+
+        # get sliced prediction
+        prediction_result = get_sliced_prediction(
+            image=image_path,
+            detection_model=torchvision_detection_model,
+            slice_height=slice_height,
+            slice_width=slice_width,
+            overlap_height_ratio=overlap_height_ratio,
+            overlap_width_ratio=overlap_width_ratio,
+            perform_standard_pred=False,
+            postprocess_type=postprocess_type,
+            postprocess_match_threshold=match_threshold,
+            postprocess_match_metric=match_metric,
+            postprocess_class_agnostic=class_agnostic,
+        )
+        object_prediction_list = prediction_result.object_prediction_list
+
+        # compare
+        self.assertEqual(len(object_prediction_list), 18)
+        self.assertEqual(object_prediction_list[0].category.id, 3)
+        self.assertEqual(object_prediction_list[0].category.name, "car")
+        self.assertEqual(object_prediction_list[0].bbox.to_coco_bbox(), [765, 259, 29, 25])
+
+
+if __name__ == "__main__":
+    unittest.main()
