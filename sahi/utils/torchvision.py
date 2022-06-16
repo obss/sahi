f4ee38c8d17ef560728c8d97ab821892e86da9b6
fcakyon <fcakyon@gmail.com>
2022-06-16 20:18:52 +0300

    reafactor torchvision utils

--- sahi/utils/torchvision.py
+++ sahi/utils/torchvision.py
@@ -1,39 +1,27 @@
 # OBSS SAHI Tool
 # Code written by Kadir Nar, 2022.
 
-import urllib.request
-from os import path
-from pathlib import Path
-from typing import Optional
 
-import cv2
-import numpy as np
-import torchvision
-from PIL import Image
-from torchvision.io.image import read_image
+from sahi.utils.import_utils import is_available
 
 
 class TorchVisionTestConstants:
-    FASTERCNN_CONFIG_ZOO_NAME = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    # RETINANET_CONFIG_ZOO_NAME = detection.retinanet_resnet50_fpn(pretrained=True)
-    # MASKRCNN_CONFIG_ZOO_NAME = detection.maskrcnn_resnet50_fpn(pretrained=True)
-
-    FASTERCNN_CONFIG_URL = "https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth"
-    FASTERCNN_MODEL_PATH = "tests/data/models/torcvhvision/faster_rcnn.pth"
+    FASTERRCNN_CONFIG_PATH = "tests/data/models/torchvision/fasterrcnn_resnet50_fpn.yaml"
+    SSD300_CONFIG_PATH = "tests/data/models/torchvision/ssd300_vgg16.yaml"
 
 
-def download_torchvision_model(destination_path: Optional[str] = None):
-    if destination_path is None:
-        destination_path = TorchVisionTestConstants.FASTERCNN_MODEL_PATH
+if is_available("torchvision"):
+    import torchvision
 
-    Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
-
-    if not path.exists(destination_path):
-        urllib.request.urlretrieve(
-            TorchVisionTestConstants.FASTERCNN_CONFIG_URL,
-            destination_path,
-        )
-
+    MODEL_NAME_TO_CONSTRUCTOR = {
+        "fasterrcnn_resnet50_fpn": torchvision.models.detection.fasterrcnn_resnet50_fpn,
+        "fasterrcnn_mobilenet_v3_large_fpn": torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn,
+        "fasterrcnn_mobilenet_v3_large_320_fpn": torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn,
+        "fcos_resnet50_fpn": torchvision.models.detection.fcos_resnet50_fpn,
+        "retinanet_resnet50_fpn": torchvision.models.detection.retinanet_resnet50_fpn,
+        "ssd300_vgg16": torchvision.models.detection.ssd300_vgg16,
+        "ssdlite320_mobilenet_v3_large": torchvision.models.detection.ssdlite320_mobilenet_v3_large,
+    }
 
 COCO_CLASSES = [
     "__background__",
@@ -128,36 +116,3 @@ COCO_CLASSES = [
     "hair drier",
     "toothbrush",
 ]
-
-
-def numpy_to_pil(img):
-    return Image.fromarray(img)
-
-
-def pil_to_numpy(img):
-    import numpy as np
-
-    return np.array(img)
-
-
-def resize_aspect_ratio(img, long_size):
-    height, width, channel = img.shape
-
-    # set target image size
-    target_size = long_size
-
-    ratio = target_size / max(height, width)
-
-    target_h, target_w = int(height * ratio), int(width * ratio)
-    proc = cv2.resize(img, (target_w, target_h), interpolation=cv2.INTER_LINEAR)
-
-    # make canvas and paste image
-    target_h32, target_w32 = target_h, target_w
-    if target_h % 32 != 0:
-        target_h32 = target_h + (32 - target_h % 32)
-    if target_w % 32 != 0:
-        target_w32 = target_w + (32 - target_w % 32)
-    resized = np.zeros((target_h32, target_w32, channel), dtype=np.float32)
-    resized[0:target_h, 0:target_w, :] = proc
-
-    return resized
