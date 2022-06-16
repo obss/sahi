268da8dc63fdf6f0313570b423b297b04617e00e
Devrim <46989091+devrimcavusoglu@users.noreply.github.com>
2022-06-17 01:23:35 +0300

    add support for huggingface transformers object detectors (#475)
    * HFTransformers support added.
    
    * winpy error on torch (moving on linux for now).
    
    * class name is renamed as `HuggingfaceDetectionModel`.
    - Notebook for huggingface detection model is prepared.
    - Image size option is implemented in feature extractor.
    - pybboxes is added for bounding box conversion (currently from yolo to voc).
    
    * Removed redundant "scale_factor".
    
    * HFTransformers support added.
    
    * winpy error on torch (moving on linux for now).
    
    * class name is renamed as `HuggingfaceDetectionModel`.
    - Notebook for huggingface detection model is prepared.
    - Image size option is implemented in feature extractor.
    - pybboxes is added for bounding box conversion (currently from yolo to voc).
    
    * Removed redundant "scale_factor".
    
    * Test is added for HuggingfaceDetectionModel.
    
    * Typo fix in test cases.
    
    * Model loading from str moved to `load_model()`.
    
    * set_model() is fixed, feature_extractor arg added to the constructor.
    - Bug fix in test.
    
    * Bug fix in set_model(), check from model class name.
    
    * Test data path is corrected.
    
    * Apply suggestions from code review
    
    Co-authored-by: fatih <34196005+fcakyon@users.noreply.github.com>
    
    * Notebook updated.
    - Bug fix in image w & h sizes.
    
    * Code formatting.
    
    * Test cases fixed image w x h.
    
    * Typo correction in test demo path.
    
    * Test cases added for get_prediction, get_sliced_prediction and automodel for HuggingfaceDetectionModel.
    
    * minor updates on hf demo notebook
    
    * add huggingface support to readme
    
    * utilize check_requirements decorator
    
    * fix linting
    
    * fix class init
    
    * fix layer tests
    
    * fix torch dependency in model.py
    
    Co-authored-by: fatih <34196005+fcakyon@users.noreply.github.com>
    Co-authored-by: fcakyon <fcakyon@gmail.com>

--- sahi/model.py
+++ sahi/model.py
@@ -3,9 +3,10 @@
 
 import logging
 import warnings
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import numpy as np
+import pybboxes.functional as pbf
 
 from sahi.prediction import ObjectPrediction
 from sahi.utils.compatibility import fix_full_shape_list, fix_shift_amount_list
@@ -83,7 +84,7 @@ class DetectionModel:
         """
         raise NotImplementedError()
 
-    def set_model(self, model: Any):
+    def set_model(self, model: Any, **kwargs):
         """
         This function should be implemented to instantiate a DetectionModel out of an already loaded model
         Args:
@@ -180,6 +181,7 @@ class DetectionModel:
         return self._original_predictions
 
 
+@check_requirements(["torch", "mmdet", "mmcv"])
 class MmdetDetectionModel(DetectionModel):
     def load_model(self):
         """
@@ -374,15 +376,13 @@ class MmdetDetectionModel(DetectionModel):
         self._object_prediction_list_per_image = object_prediction_list_per_image
 
 
+@check_requirements(["torch", "yolov5"])
 class Yolov5DetectionModel(DetectionModel):
     def load_model(self):
         """
         Detection model is initialized and set to self.model.
         """
-        try:
-            import yolov5
-        except ImportError:
-            raise ImportError('Please run "pip install -U yolov5" ' "to install YOLOv5 first for YOLOv5 inference.")
+        import yolov5
 
         try:
             model = yolov5.load(self.model_path, device=self.device)
@@ -418,10 +418,6 @@ class Yolov5DetectionModel(DetectionModel):
             image_size: int
                 Inference input size.
         """
-        try:
-            import yolov5
-        except ImportError:
-            raise ImportError('Please run "pip install -U yolov5" ' "to install YOLOv5 first for YOLOv5 inference.")
 
         # Confirm model is loaded
         assert self.model is not None, "Model is not loaded, load it by calling .load_model()"
@@ -527,17 +523,9 @@ class Yolov5DetectionModel(DetectionModel):
         self._object_prediction_list_per_image = object_prediction_list_per_image
 
 
+@check_requirements(["torch", "detectron2"])
 class Detectron2DetectionModel(DetectionModel):
     def load_model(self):
-        try:
-            import detectron2
-        except ImportError:
-            raise ImportError(
-                "Please install detectron2. Check "
-                "`https://detectron2.readthedocs.io/en/latest/tutorials/install.html` "
-                "for instalattion details."
-            )
-
         from detectron2.config import get_cfg
         from detectron2.data import MetadataCatalog
         from detectron2.engine import DefaultPredictor
@@ -596,10 +584,6 @@ class Detectron2DetectionModel(DetectionModel):
             image: np.ndarray
                 A numpy array that contains the image to be predicted. 3 channel image should be in RGB order.
         """
-        try:
-            import detectron2
-        except ImportError:
-            raise ImportError("Please install detectron2 via `pip install detectron2`")
 
         # confirm image_size is not provided
         if image_size is not None:
@@ -701,5 +685,197 @@ class Detectron2DetectionModel(DetectionModel):
 
         # detectron2 DefaultPredictor supports single image
         object_prediction_list_per_image = [object_prediction_list]
+
+        self._object_prediction_list_per_image = object_prediction_list_per_image
+
+
+@check_requirements(["torch", "transformers"])
+class HuggingfaceDetectionModel(DetectionModel):
+    import torch
+
+    def __init__(
+        self,
+        model_path: Optional[str] = None,
+        model: Optional[Any] = None,
+        feature_extractor: Optional[Any] = None,
+        config_path: Optional[str] = None,
+        device: Optional[str] = None,
+        mask_threshold: float = 0.5,
+        confidence_threshold: float = 0.3,
+        category_mapping: Optional[Dict] = None,
+        category_remapping: Optional[Dict] = None,
+        load_at_init: bool = True,
+        image_size: int = None,
+    ):
+        self._feature_extractor = feature_extractor
+        self._image_shapes = []
+        super().__init__(
+            model_path,
+            model,
+            config_path,
+            device,
+            mask_threshold,
+            confidence_threshold,
+            category_mapping,
+            category_remapping,
+            load_at_init,
+            image_size,
+        )
+
+    @property
+    def feature_extractor(self):
+        return self._feature_extractor
+
+    @property
+    def image_shapes(self):
+        return self._image_shapes
+
+    @property
+    def num_categories(self) -> int:
+        """
+        Returns number of categories
+        """
+        return self.model.config.num_labels
+
+    def load_model(self):
+        from transformers import AutoFeatureExtractor, AutoModelForObjectDetection
+
+        model = AutoModelForObjectDetection.from_pretrained(self.model_path)
+        if self.image_size is not None:
+            feature_extractor = AutoFeatureExtractor.from_pretrained(
+                self.model_path, size=self.image_size, do_resize=True
+            )
+        else:
+            feature_extractor = AutoFeatureExtractor.from_pretrained(self.model_path)
+        self.set_model(model, feature_extractor)
+
+    def set_model(self, model: Any, feature_extractor: Any = None):
+        feature_extractor = feature_extractor or self.feature_extractor
+        if feature_extractor is None:
+            raise ValueError(f"'feature_extractor' is required to be set, got {feature_extractor}.")
+        elif (
+            "ObjectDetection" not in model.__class__.__name__
+            or "FeatureExtractor" not in feature_extractor.__class__.__name__
+        ):
+            raise ValueError(
+                f"Given 'model' is not an ObjectDetectionModel or 'feature_extractor' is not a valid FeatureExtractor."
+            )
+        self.model = model
+        self.model.to(self.device)
+        self._feature_extractor = feature_extractor
+        self.category_mapping = self.model.config.id2label
+
+    def perform_inference(self, image: Union[List, np.ndarray], image_size: int = None):
+        """
+        Prediction is performed using self.model and the prediction result is set to self._original_predictions.
+        Args:
+            image: np.ndarray
+                A numpy array that contains the image to be predicted. 3 channel image should be in RGB order.
+        """
+        import torch
+
+        # confirm image_size is not provided
+        if image_size is not None:
+            warnings.warn("Set 'image_size' at DetectionModel init.")
+
+        # Confirm model is loaded
+        if self.model is None:
+            raise RuntimeError("Model is not loaded, load it by calling .load_model()")
+
+        with torch.no_grad():
+            inputs = self.feature_extractor(images=image, return_tensors="pt")
+            inputs["pixel_values"] = inputs.pixel_values.to(self.device)
+            if hasattr(inputs, "pixel_mask"):
+                inputs["pixel_mask"] = inputs.pixel_mask.to(self.device)
+            outputs = self.model(**inputs)
+
+        if isinstance(image, list):
+            self._image_shapes = [img.shape for img in image]
+        else:
+            self._image_shapes = [image.shape]
+        self._original_predictions = outputs
+
+    def get_valid_predictions(
+        self, logits: torch.Tensor, pred_boxes: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        import torch
+
+        probs = logits.softmax(-1)
+        scores = probs.max(-1).values
+        cat_ids = probs.argmax(-1)
+        valid_detections = torch.where(cat_ids < self.num_categories, 1, 0)
+        valid_confidences = torch.where(scores >= self.confidence_threshold, 1, 0)
+        valid_mask = valid_detections.logical_and(valid_confidences)
+        scores = scores[valid_mask]
+        cat_ids = cat_ids[valid_mask]
+        boxes = pred_boxes[valid_mask]
+        return scores, cat_ids, boxes
+
+    def _create_object_prediction_list_from_original_predictions(
+        self,
+        shift_amount_list: Optional[List[List[int]]] = [[0, 0]],
+        full_shape_list: Optional[List[List[int]]] = None,
+    ):
+        """
+        self._original_predictions is converted to a list of prediction.ObjectPrediction and set to
+        self._object_prediction_list_per_image.
+        Args:
+            shift_amount_list: list of list
+                To shift the box and mask predictions from sliced image to full sized image, should
+                be in the form of List[[shift_x, shift_y],[shift_x, shift_y],...]
+            full_shape_list: list of list
+                Size of the full image after shifting, should be in the form of
+                List[[height, width],[height, width],...]
+        """
+        original_predictions = self._original_predictions
+
+        # compatilibty for sahi v0.8.15
+        shift_amount_list = fix_shift_amount_list(shift_amount_list)
+        full_shape_list = fix_full_shape_list(full_shape_list)
+
+        n_image = original_predictions.logits.shape[0]
+        object_prediction_list_per_image = []
+        for image_ind in range(n_image):
+            image_height, image_width, _ = self.image_shapes[image_ind]
+            scores, cat_ids, boxes = self.get_valid_predictions(
+                logits=original_predictions.logits[image_ind], pred_boxes=original_predictions.pred_boxes[image_ind]
+            )
+
+            # create object_prediction_list
+            object_prediction_list = []
+
+            shift_amount = shift_amount_list[image_ind]
+            full_shape = None if full_shape_list is None else full_shape_list[image_ind]
+
+            for ind in range(len(boxes)):
+                category_id = cat_ids[ind].item()
+                yolo_bbox = boxes[ind].tolist()
+                bbox = list(
+                    pbf.convert_bbox(
+                        yolo_bbox,
+                        from_type="yolo",
+                        to_type="voc",
+                        image_size=(image_width, image_height),
+                        return_values=True,
+                    )
+                )
+
+                # fix negative box coords
+                bbox[0] = max(0, int(bbox[0]))
+                bbox[1] = max(0, int(bbox[1]))
+                bbox[2] = min(bbox[2], image_width)
+                bbox[3] = min(bbox[3], image_height)
+
+                object_prediction = ObjectPrediction(
+                    bbox=bbox,
+                    bool_mask=None,
+                    category_id=category_id,
+                    category_name=self.category_mapping[category_id],
+                    shift_amount=shift_amount,
+                    score=scores[ind].item(),
+                    full_shape=full_shape,
+                )
+                object_prediction_list.append(object_prediction)
+            object_prediction_list_per_image.append(object_prediction_list)
 
         self._object_prediction_list_per_image = object_prediction_list_per_image
---
+++ tests/test_huggingfacemodel.py
@@ -0,0 +1,144 @@
+# OBSS SAHI Tool
+# Code written by Devrim Cavusoglu, 2022.
+
+import unittest
+
+import pybboxes.functional as pbf
+
+from sahi.utils.cv import read_image
+from sahi.utils.huggingface import HuggingfaceTestConstants
+
+MODEL_DEVICE = "cpu"
+CONFIDENCE_THRESHOLD = 0.3
+IMAGE_SIZE = 320
+
+
+class TestHuggingfaceDetectionModel(unittest.TestCase):
+    def test_load_model(self):
+        from sahi.model import HuggingfaceDetectionModel
+
+        huggingface_detection_model = HuggingfaceDetectionModel(
+            model_path=HuggingfaceTestConstants.YOLOS_TINY_MODEL_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+        )
+
+        self.assertNotEqual(huggingface_detection_model.model, None)
+
+    def test_set_model(self):
+        from transformers import AutoFeatureExtractor, AutoModelForObjectDetection
+
+        from sahi.model import HuggingfaceDetectionModel
+
+        huggingface_model = AutoModelForObjectDetection.from_pretrained(HuggingfaceTestConstants.YOLOS_TINY_MODEL_PATH)
+        huggingface_feature_extractor = AutoFeatureExtractor.from_pretrained(
+            HuggingfaceTestConstants.YOLOS_TINY_MODEL_PATH
+        )
+
+        huggingface_detection_model = HuggingfaceDetectionModel(
+            model=huggingface_model,
+            feature_extractor=huggingface_feature_extractor,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+        )
+
+        self.assertNotEqual(huggingface_detection_model.model, None)
+
+    def test_perform_inference(self):
+        from sahi.model import HuggingfaceDetectionModel
+
+        huggingface_detection_model = HuggingfaceDetectionModel(
+            model_path=HuggingfaceTestConstants.YOLOS_TINY_MODEL_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+            image_size=IMAGE_SIZE,
+        )
+
+        # prepare image
+        image_path = "tests/data/small-vehicles1.jpeg"
+        image = read_image(image_path)
+
+        # perform inference
+        huggingface_detection_model.perform_inference(image)
+        original_predictions = huggingface_detection_model.original_predictions
+
+        scores, cat_ids, boxes = huggingface_detection_model.get_valid_predictions(
+            logits=original_predictions.logits[0], pred_boxes=original_predictions.pred_boxes[0]
+        )
+
+        # find box of first car detection with conf greater than 0.5
+        for i, box in enumerate(boxes):
+            if huggingface_detection_model.category_mapping[cat_ids[i].item()] == "car":  # if category car
+                break
+
+        image_height, image_width, _ = huggingface_detection_model.image_shapes[0]
+        box = list(
+            pbf.convert_bbox(
+                box.tolist(),
+                from_type="yolo",
+                to_type="voc",
+                image_size=(image_width, image_height),
+                return_values=True,
+            )
+        )
+
+        # compare
+        desired_bbox = [639, 198, 663, 218]
+        predicted_bbox = list(map(int, box[:4]))
+        margin = 2
+        for ind, point in enumerate(predicted_bbox):
+            assert point < desired_bbox[ind] + margin and point > desired_bbox[ind] - margin
+        for score in scores:
+            self.assertGreaterEqual(score.item(), CONFIDENCE_THRESHOLD)
+
+    def test_convert_original_predictions(self):
+        from sahi.model import HuggingfaceDetectionModel
+
+        huggingface_detection_model = HuggingfaceDetectionModel(
+            model_path=HuggingfaceTestConstants.YOLOS_TINY_MODEL_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            category_remapping=None,
+            load_at_init=True,
+            image_size=IMAGE_SIZE,
+        )
+
+        # prepare image
+        image_path = "tests/data/small-vehicles1.jpeg"
+        image = read_image(image_path)
+
+        # perform inference
+        huggingface_detection_model.perform_inference(image)
+
+        # convert predictions to ObjectPrediction list
+        huggingface_detection_model.convert_original_predictions()
+        object_prediction_list = huggingface_detection_model.object_prediction_list
+
+        # compare
+        self.assertEqual(len(object_prediction_list), 46)
+        self.assertEqual(object_prediction_list[0].category.id, 3)
+        self.assertEqual(object_prediction_list[0].category.name, "car")
+        desired_bbox = [639, 198, 24, 20]
+        predicted_bbox = object_prediction_list[0].bbox.to_coco_bbox()
+        margin = 2
+        for ind, point in enumerate(predicted_bbox):
+            assert point < desired_bbox[ind] + margin and point > desired_bbox[ind] - margin
+        self.assertEqual(object_prediction_list[2].category.id, 3)
+        self.assertEqual(object_prediction_list[2].category.name, "car")
+        desired_bbox = [663, 187, 17, 16]
+        predicted_bbox = object_prediction_list[2].bbox.to_coco_bbox()
+        for ind, point in enumerate(predicted_bbox):
+            assert point < desired_bbox[ind] + margin and point > desired_bbox[ind] - margin
+
+        for object_prediction in object_prediction_list:
+            self.assertGreaterEqual(object_prediction.score.value, CONFIDENCE_THRESHOLD)
+
+
+if __name__ == "__main__":
+    unittest.main()
