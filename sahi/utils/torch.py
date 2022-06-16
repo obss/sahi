c4e6f12dcf5b45d414878b76230648a22280ee5a
fcakyon <fcakyon@gmail.com>
2022-06-16 20:18:30 +0300

    refactor torch utils

--- sahi/utils/torch.py
+++ sahi/utils/torch.py
@@ -2,46 +2,31 @@
 # Code written by Fatih C Akyon, 2020.
 
 
-from sahi.utils.import_utils import is_torch_available
+from sahi.utils.import_utils import check_requirements, is_available
 
 
-def torch_load(path):
-    if is_torch_available():
-        import torch
-
-        return torch.load(path)
-    else:
-        raise ImportError("You need to install PyTorch to use this method.")
-
-
+@check_requirements(["torch"])
 def empty_cuda_cache():
     if is_torch_cuda_available():
         import torch
 
-        return empty_cuda_cache()
+        return torch.cuda.empty_cache()
     else:
-        raise ImportError("You need to install PyTorch to use this method.")
+        raise RuntimeError("CUDA not available.")
 
 
-def torch_stack():
-    if is_torch_available():
-        import torch
+if is_available("torch"):
 
-        return torch_stack
-    else:
-        raise ImportError("You need to install PyTorch to use this method.")
-
-
-def to_float_tensor(img):
-    """
-    Converts a PIL.Image (RGB) or numpy.ndarray (H x W x C) in the range
-    [0, 255] to a torch.FloatTensor of shape (C x H x W).
-    Args:
-        img: np.ndarray
-    Returns:
-        torch.tensor
-    """
-    if is_torch_available():
+    @check_requirements(["torch"])
+    def to_float_tensor(img):
+        """
+        Converts a PIL.Image (RGB) or numpy.ndarray (H x W x C) in the range
+        [0, 255] to a torch.FloatTensor of shape (C x H x W).
+        Args:
+            img: np.ndarray
+        Returns:
+            torch.tensor
+        """
         import torch
 
         img = img.transpose((2, 0, 1))
@@ -51,24 +36,18 @@ def to_float_tensor(img):
 
         return img
 
-    else:
-        raise ImportError("You need to install PyTorch to use this method.")
-
-
-def torch_to_numpy(img):
-    if is_torch_available():
+    @check_requirements(["torch"])
+    def torch_to_numpy(img):
         import torch
 
         img = img.numpy()
         if img.max() > 1:
             img /= 255
         return img.transpose((1, 2, 0))
-    else:
-        raise ImportError("You need to install PyTorch to use this method.")
 
 
 def is_torch_cuda_available():
-    if is_torch_available():
+    if is_available("torch"):
         import torch
 
         return torch.cuda.is_available()
