7179f6832dd602e7c3a1171945b4d9fcfeecfaf3
kadirnar <kadir.nar@hotmail.com>
2022-06-19 01:55:25 +0300

    SAHI için tensorflow_hub kütüphanesi eklendi.

--- sahi/model.py
+++ sahi/model.py
@@ -1063,3 +1063,96 @@ class TorchVisionDetectionModel(DetectionModel):
             object_prediction_list_per_image.append(object_prediction_list)
 
         self._object_prediction_list_per_image = object_prediction_list_per_image
+
+@check_requirements(["tensorflow","tensorflow_hub"])
+class TensorflowHubDetectionModel(DetectionModel):
+    def load_model(self):
+        try:
+            import tensorflow_hub as hub
+            
+        except ImportError:
+            raise ImportError("Please install tensorflow and tensorflow_hub via `pip install tensorflow' and 'pip install tensorflow_hub`")
+
+        self.model = hub.load(self.model_path)
+        
+    def perform_inference(self, image: np.ndarray):
+        if self.image_size is not None:
+            from sahi.utils.tfhub import get_image,resize
+            
+            img = get_image(image)
+            img = resize(img, self.image_size)
+            
+            prediction_result = self.model(img)
+        
+        else:
+            from sahi.utils.tfhub import get_image
+            img = get_image(image)
+            
+            prediction_result = self.model(img)
+        
+        self._original_predictions = prediction_result
+        self.img = get_image(image)
+        
+        if self.category_mapping is None:
+            from sahi.utils.tfhub import COCO_CLASSES
+            category_mapping = {str(i): COCO_CLASSES[i] for i in range(len(COCO_CLASSES))}
+            self.category_mapping = category_mapping 
+        
+    @property
+    def num_categories(self):
+        num_categories = len(self.category_mapping)
+        return num_categories
+    
+    @property
+    def has_mask(self):
+        return self.model.with_mask
+    
+    @property
+    def category_names(self):     
+        return list(self.category_mapping.values()) 
+    
+    def _create_object_prediction_list_from_original_predictions(
+        self,
+        shift_amount_list: Optional[List[List[int]]] = [[0, 0]],
+        full_shape_list: Optional[List[List[int]]] = None,
+    ): 
+        original_predictions = self._original_predictions
+        
+        # compatilibty for sahi v0.8.20
+        if isinstance(shift_amount_list[0], int):
+            shift_amount_list = [shift_amount_list]
+        if full_shape_list is not None and isinstance(full_shape_list[0], int):
+            full_shape_list = [full_shape_list]
+            
+        shift_amount = shift_amount_list[0]
+        full_shape = None if full_shape_list is None else full_shape_list[0]
+            
+        boxes = original_predictions["detection_boxes"][0]
+        scores = original_predictions["detection_scores"][0]
+        category_ids = original_predictions["detection_classes"][0]
+        
+        # create object_prediction_list
+        object_prediction_list = []
+        object_prediction_list_per_image = []
+
+        for i in range(min(boxes.shape[0], 100)):
+            if scores[i] >= self.confidence_threshold:
+                score = float(scores[i].numpy())
+                category_id = int(category_ids[i].numpy())
+                category_names = self.category_mapping[str(category_id-1)]
+                box = [float(box) for box in boxes[i].numpy()]
+                x1, y1, x2, y2 = int(box[1] * self.img.shape[2]), int(box[0] * self.img.shape[1]), int(box[3] * self.img.shape[2]), int(box[2] * self.img.shape[1])
+                bbox = [x1, y1, x2, y2]
+
+                object_prediction = ObjectPrediction(
+                    bbox = bbox,
+                    bool_mask=None,
+                    category_id= category_id,
+                    category_name= category_names,
+                    shift_amount=shift_amount,
+                    score= score,
+                    full_shape=full_shape,
+                )
+                object_prediction_list.append(object_prediction)
+        object_prediction_list_per_image.append(object_prediction_list)
+        self._object_prediction_list_per_image = object_prediction_list_per_image                   
---
+++ sahi/utils/tfhub.py
@@ -0,0 +1,100 @@
+def get_image(array):
+    import tensorflow as tf
+    import numpy as np
+    
+    array = np.asarray(array, np.float32)
+    normalized_array = array
+    if array.max() <= 1:
+        normalized_array = array * 255.0
+
+    normalized_array = np.asarray(normalized_array, np.uint8)
+    normalized_array = tf.convert_to_tensor([normalized_array], tf.uint8)
+    return normalized_array
+
+def resize(array, size):
+    import tensorflow as tf 
+    return tf.image.resize(array, [size,size]).numpy()
+
+
+COCO_CLASSES = (
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "dining table",
+    "toilet",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+)
