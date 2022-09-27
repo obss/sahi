6478f5218954490caa18af0ad6f4079d8b43ce16
fcakyon <fcakyon@gmail.com>
2022-09-27 17:07:35 +0300

    make pycocotools optional

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setuptools.setup(
     python_requires=">=3.6",
     install_requires=get_requirements(),
     extras_require={
-        "tests": ["pytest", "mmdet", "norfair", "layer"],
+        "tests": ["mmdet==2.25.1", "norfair==1.0.0", "layer", "pycocotools==2.0.5"],
         "dev": ["black==21.7b0", "flake8==3.9.2", "isort==5.9.2", "jupyterlab==3.0.14"],
     },
     classifiers=[
