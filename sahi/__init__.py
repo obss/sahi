294b27374a5564b73bd9ea53f4eee02eae27cbb9
fatih <34196005+fcakyon@users.noreply.github.com>
2022-11-15 13:23:51 +0300

    make some classes importable from highest level (#731)

--- sahi/__init__.py
+++ sahi/__init__.py
@@ -1,3 +1,6 @@
 __version__ = "0.11.4"
 
+from sahi.annotation import BoundingBox, Category, Mask
 from sahi.auto_model import AutoDetectionModel
+from sahi.models.base import DetectionModel
+from sahi.prediction import ObjectPrediction
