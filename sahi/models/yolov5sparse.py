d2ae236610904bbc185bca9883b8f6b8db1d942f
Derrick Mwiti <mwitiderrick@gmail.com>
2023-03-13 13:21:15 +0300

    Update yolov5sparse.py

--- sahi/models/yolov5sparse.py
+++ sahi/models/yolov5sparse.py
@@ -75,10 +75,8 @@ class Yolov5SparseDetectionModel(DetectionModel):
 
     @property
     def category_names(self):
-        if check_package_minimum_version("yolov5", "6.2.0"):
-            return list(self.model.names.values())
-        else:
-            return self.model.names
+        return ['person','bicycle','car','motorcycle','airplane','bus','train','truck','boat','traffic light','fire hydrant','stop sign', 'parking meter','bench','bird','cat','dog','horse','sheep','cow','elephant','bear','zebra','giraffe' 'backpack','umbrella','handbag','tie','suitcase','frisbee','skis','snowboard','sports ball','kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket','bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple','sandwich','orange','broccoli','carrot','hot dog','pizza','donut','cake','chair','couch','potted plant','bed','dining table','toilet','tv','laptop','mouse','remote','keyboard','cell phone','microwave','oven','toaster','sink','refrigerator','book','clock','vase','scissors','teddy bear','hair drier','toothbrush']
+
 
     def _create_object_prediction_list_from_original_predictions(
         self,
