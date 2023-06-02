5c33f2d3c2e16b59e3a748ff329ce44922cd86cb
Pranav Durai <pranavdurai10@gmail.com>
2023-06-02 12:58:44 +0530

    Update yolov8.py
    Added 'download_yolov8_model' function support for YOLOv8-M model.

--- sahi/utils/yolov8.py
+++ sahi/utils/yolov8.py
@@ -41,3 +41,16 @@ def download_yolov8s_model(destination_path: Optional[str] = None):
             Yolov8TestConstants.YOLOV8S_MODEL_URL,
             destination_path,
         )
+
+def download_yolov8m_model(destination_path: Optional[str] = None):
+
+    if destination_path is None:
+        destination_path = Yolov8TestConstants.YOLOV8M_MODEL_PATH
+
+    Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
+
+    if not path.exists(destination_path):
+        urllib.request.urlretrieve(
+            Yolov8TestConstants.YOLOV8M_MODEL_URL,
+            destination_path,
+        )
