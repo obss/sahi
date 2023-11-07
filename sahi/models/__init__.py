3e02b639a1f677badeb9a7d359f07c4579d05a06
Karl-Joan Alesma <karl.joan.alesma@gmail.com>
2023-11-07 17:39:54 +0200

    Fix code formatting

--- sahi/models/__init__.py
+++ sahi/models/__init__.py
@@ -1,1 +1,1 @@
-from . import base, detectron2, huggingface, mmdet, yolov8onnx, torchvision, yolonas, yolov5
+from . import base, detectron2, huggingface, mmdet, torchvision, yolonas, yolov5, yolov8onnx
