3f50a77aa000cf3d7ff117019ed49ca07fc32305
devrimcavusoglu <devrimcavusoglu@gmail.com>
2024-01-08 21:59:18 +0300

    RTDETR unit tests added, unused imports removed.

--- sahi/utils/rtdetr.py
+++ sahi/utils/rtdetr.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Optional
 
 
-class Yolov8TestConstants:
+class RTDETRTestConstants:
     RTDETRL_MODEL_URL = "https://github.com/ultralytics/assets/releases/download/v0.0.0/rtdetr-l.pt"
     RTDETRL_MODEL_PATH = "tests/data/models/rtdetr/rtdetr-l.pt"
 
@@ -14,25 +14,25 @@ class Yolov8TestConstants:
 
 def download_rtdetrl_model(destination_path: Optional[str] = None):
     if destination_path is None:
-        destination_path = Yolov8TestConstants.RTDETRL_MODEL_PATH
+        destination_path = RTDETRTestConstants.RTDETRL_MODEL_PATH
 
     Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
 
     if not path.exists(destination_path):
         urllib.request.urlretrieve(
-            Yolov8TestConstants.RTDETRX_MODEL_URL,
+            RTDETRTestConstants.RTDETRX_MODEL_URL,
             destination_path,
         )
 
 
 def download_rtdetrx_model(destination_path: Optional[str] = None):
     if destination_path is None:
-        destination_path = Yolov8TestConstants.RTDETRX_MODEL_PATH
+        destination_path = RTDETRTestConstants.RTDETRX_MODEL_PATH
 
     Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
 
     if not path.exists(destination_path):
         urllib.request.urlretrieve(
-            Yolov8TestConstants.RTDETRX_MODEL_URL,
+            RTDETRTestConstants.RTDETRX_MODEL_URL,
             destination_path,
         )
