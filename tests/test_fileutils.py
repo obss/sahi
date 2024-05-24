27275baa2f1e9b8ef232c7520ec6e6d833379569
Mark Merling <mark.merling@capitalone.com>
2024-05-24 13:10:50 -0400

    adding in new unit tests

--- tests/test_fileutils.py
+++ tests/test_fileutils.py
@@ -2,6 +2,7 @@
 # Code written by Fatih C Akyon, 2020.
 
 import unittest
+from unittest.mock import patch
 
 
 class TestFileUtils(unittest.TestCase):
@@ -21,6 +22,16 @@ class TestFileUtils(unittest.TestCase):
         )
         self.assertEqual(len(relative_filepath_list), 7)
         self.assertEqual(len(abs_filepath_list), 7)
+
+    def test_increment_path(self):
+        from sahi.utils.file import increment_path
+
+        with patch("sahi.utils.file.Path.exists", return_value=False):
+            path = increment_path("test.txt")
+            self.assertEqual(path, "test.txt")
+        with patch("sahi.utils.file.Path.exists", return_value=True):
+            path = increment_path("test.txt", exist_ok=False)
+            self.assertEqual(path, "test.txt2")
 
 
 if __name__ == "__main__":
---
+++ tests/test_prediction.py
@@ -0,0 +1,22 @@
+# OBSS SAHI Tool
+# Code written by Fatih C Akyon, 2020.
+
+import unittest
+from unittest.mock import MagicMock
+
+import numpy as np
+
+from sahi.prediction import PredictionScore
+
+
+class TestPrediction(unittest.TestCase):
+    def test_prediction_score(self):
+
+        prediction_score = PredictionScore(np.array(0.6))
+        self.assertEqual(type(prediction_score.value), float)
+        self.assertEqual(prediction_score.is_greater_than_threshold(0.5), True)
+        self.assertEqual(prediction_score.is_greater_than_threshold(0.7), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
