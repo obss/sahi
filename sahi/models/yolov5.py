0191fbbe6b97e6aae53dd4b964952d62c11cefc9
Janne Mäyrä <22835847+mayrajeo@users.noreply.github.com>
2024-06-02 18:37:14 +0300

    add yolov8 mask support, improve mask processing speed by 4-5x (#1039)
    Co-authored-by: fatih c. akyon <34196005+fcakyon@users.noreply.github.com>

--- sahi/models/yolov5.py
+++ sahi/models/yolov5.py
@@ -157,7 +157,7 @@ class Yolov5DetectionModel(DetectionModel):
                     bbox=bbox,
                     category_id=category_id,
                     score=score,
-                    bool_mask=None,
+                    segmentation=None,
                     category_name=category_name,
                     shift_amount=shift_amount,
                     full_shape=full_shape,
