0bef0ef64e3584891b89b149cef7ac7c25a40367
fatih c. akyon <34196005+fcakyon@users.noreply.github.com>
2024-11-22 09:55:41 +0300

    Update auto_model.py

--- sahi/auto_model.py
+++ sahi/auto_model.py
@@ -11,7 +11,6 @@ MODEL_TYPE_TO_MODEL_CLASS_NAME = {
     "huggingface": "HuggingfaceDetectionModel",
     "torchvision": "TorchVisionDetectionModel",
     "yolov5sparse": "Yolov5SparseDetectionModel",
-    "yolonas": "YoloNasDetectionModel",
     "yolov8onnx": "Yolov8OnnxDetectionModel",
 }
 
