6647a16ddd2098ad8d0967e72a8998d8b50040b2
fcakyon <fcakyon@gmail.com>
2024-12-16 16:15:18 +0300

    simplify yolo detection model code

---
+++ sahi/modelsv2/mmdet.py
@@ -0,0 +1,202 @@
+# OBSS SAHI Tool
+# Code written by Fatih C Akyon, 2020.
+
+import logging
+from typing import Any, List, Optional
+
+import numpy as np
+from PIL import Image
+
+from sahi.models.base import DetectionModel
+from sahi.prediction import ObjectPrediction
+from sahi.utils.cv import get_bbox_from_bool_mask
+from sahi.utils.import_utils import check_requirements
+
+logger = logging.getLogger(__name__)
+
+
+class MmdetDetectionModel(DetectionModel):
+    def check_dependencies(self):
+        check_requirements(["torch", "mmdet", "mmcv"])
+
+    def load_model(self):
+        """
+        Detection model is initialized and set to self.model.
+        """
+        from mmdet.apis import init_detector
+
+        # create model
+        model = init_detector(
+            config=self.config_path,
+            checkpoint=self.model_path,
+            device=self.device,
+        )
+
+        # update model image size
+        if self.image_size is not None:
+            model.cfg.data.test.pipeline[1]["img_scale"] = (self.image_size, self.image_size)
+
+        self.set_model(model)
+
+    def set_model(self, model: Any):
+        """
+        Sets the underlying MMDetection model.
+        Args:
+            model: Any
+                A MMDetection model
+        """
+
+        # set self.model
+        self.model = model
+
+        # set category_mapping
+        if not self.category_mapping:
+            category_mapping = {str(ind): category_name for ind, category_name in enumerate(self.category_names)}
+            self.category_mapping = category_mapping
+
+    def perform_inference(self, images: List):
+        """
+        Prediction is performed using self.model and the prediction result is set to self._original_predictions.
+        Args:
+            images: List[np.ndarray, str, PIL.Image.Image]
+                A numpy array that contains a list of images to be predicted. 3 channel image should be in RGB order.
+        """
+
+        if not isinstance(images, list):
+            images = [images]
+
+        # Confirm model is loaded
+        if self.model is None:
+            raise ValueError("Model is not loaded, load it by calling .load_model()")
+        # Supports only batch of 1
+        from mmdet.apis import inference_detector
+
+        # perform inference
+        for ind, image in enumerate(images):
+            if isinstance(image, Image.Image):
+                image = np.array(image)
+            if isinstance(image, np.ndarray):
+                # mmdet accepts in BGR order
+                images[ind] = image[..., ::-1]
+
+        prediction_result = inference_detector(self.model, images)
+
+        self._original_predictions = prediction_result
+
+    @property
+    def num_categories(self):
+        """
+        Returns number of categories
+        """
+        if isinstance(self.model.CLASSES, str):
+            num_categories = 1
+        else:
+            num_categories = len(self.model.CLASSES)
+        return num_categories
+
+    @property
+    def has_mask(self):
+        """
+        Returns if model output contains segmentation mask
+        """
+        has_mask = self.model.with_mask
+        return has_mask
+
+    @property
+    def category_names(self):
+        if type(self.model.CLASSES) == str:
+            # https://github.com/open-mmlab/mmdetection/pull/4973
+            return (self.model.CLASSES,)
+        else:
+            return self.model.CLASSES
+
+    def _create_object_predictions_from_original_predictions(
+        self,
+        offset_amounts: Optional[List[List[int]]] = [[0, 0]],
+        full_shapes: Optional[List[List[int]]] = None,
+    ):
+        """
+        self._original_predictions is converted to a list of prediction.ObjectPrediction and set to
+        self._object_predictions_per_image.
+        Args:
+            offset_amounts: list of list
+                To remap the box and mask predictions from sliced image to full sized image, should
+                be in the form of List[[offset_x, offset_y],[offset_x, offset_y],...]
+            full_shapes: list of list
+                Size of the full image after remapping, should be in the form of
+                List[[height, width],[height, width],...]
+        """
+        original_predictions = self._original_predictions
+        category_mapping = self.category_mapping
+
+        # parse boxes and masks from predictions
+        num_categories = self.num_categories
+        object_predictions_per_image = []
+        for image_ind, original_prediction in enumerate(original_predictions):
+            offset_amount = offset_amounts[image_ind]
+            full_shape = None if full_shapes is None else full_shapes[image_ind]
+
+            if self.has_mask:
+                boxes = original_prediction[0]
+                masks = original_prediction[1]
+            else:
+                boxes = original_prediction
+
+            object_predictions = []
+
+            # process predictions
+            for category_id in range(num_categories):
+                category_boxes = boxes[category_id]
+                if self.has_mask:
+                    category_masks = masks[category_id]
+                num_category_predictions = len(category_boxes)
+
+                for category_predictions_ind in range(num_category_predictions):
+                    bbox = category_boxes[category_predictions_ind][:4]
+                    score = category_boxes[category_predictions_ind][4]
+                    category_name = category_mapping[str(category_id)]
+
+                    # ignore low scored predictions
+                    if score < self.confidence_threshold:
+                        continue
+
+                    # parse prediction mask
+                    if self.has_mask:
+                        bool_mask = category_masks[category_predictions_ind]
+                        # check if mask is valid
+                        # https://github.com/obss/sahi/discussions/696
+                        if get_bbox_from_bool_mask(bool_mask) is None:
+                            continue
+                    else:
+                        bool_mask = None
+
+                    # fix negative box coords
+                    bbox[0] = max(0, bbox[0])
+                    bbox[1] = max(0, bbox[1])
+                    bbox[2] = max(0, bbox[2])
+                    bbox[3] = max(0, bbox[3])
+
+                    # fix out of image box coords
+                    if full_shape is not None:
+                        bbox[0] = min(full_shape[1], bbox[0])
+                        bbox[1] = min(full_shape[0], bbox[1])
+                        bbox[2] = min(full_shape[1], bbox[2])
+                        bbox[3] = min(full_shape[0], bbox[3])
+
+                    # ignore invalid predictions
+                    if not (bbox[0] < bbox[2]) or not (bbox[1] < bbox[3]):
+                        logger.warning(f"ignoring invalid prediction with bbox: {bbox}")
+                        continue
+
+                    object_prediction = ObjectPrediction(
+                        bbox=bbox,
+                        category_id=category_id,
+                        score=score,
+                        bool_mask=bool_mask,
+                        category_name=category_name,
+                        offset_amount=offset_amount,
+                        full_shape=full_shape,
+                    )
+                    object_predictions.append(object_prediction)
+            object_predictions_per_image.append(object_predictions)
+        self._object_predictions_per_image = object_predictions_per_image
---
+++ sahi/modelsv2/torchvision.py
@@ -0,0 +1,189 @@
+# OBSS SAHI Tool
+# Code written by Fatih C Akyon and Kadir Nar, 2021.
+
+import logging
+from typing import Any, List, Optional
+
+import numpy as np
+
+from sahi.models.base import DetectionModel
+from sahi.prediction import ObjectPrediction
+from sahi.utils.import_utils import check_requirements
+
+logger = logging.getLogger(__name__)
+
+
+class TorchvisionDetectionModel(DetectionModel):
+    def check_dependencies(self) -> None:
+        check_requirements(["torch", "torchvision"])
+
+    def load_model(self):
+        import torch
+
+        from sahi.utils.torchvision import MODEL_NAME_TO_CONSTRUCTOR
+
+        # read config params
+        model_name = None
+        num_classes = None
+        if self.config_path is not None:
+            import yaml
+
+            with open(self.config_path, "r") as stream:
+                try:
+                    config = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    raise RuntimeError(exc)
+
+            model_name = config.get("model_name", None)
+            num_classes = config.get("num_classes", None)
+
+        # complete params if not provided in config
+        if not model_name:
+            model_name = "fasterrcnn_resnet50_fpn"
+            logger.warning(f"model_name not provided in config, using default model_type: {model_name}'")
+        if num_classes is None:
+            logger.warning("num_classes not provided in config, using default num_classes: 91")
+            num_classes = 91
+        if self.model_path is None:
+            logger.warning("model_path not provided in config, using pretrained weights and default num_classes: 91.")
+            pretrained = True
+            num_classes = 91
+        else:
+            pretrained = False
+
+        # load model
+        model = MODEL_NAME_TO_CONSTRUCTOR[model_name](num_classes=num_classes, pretrained=pretrained)
+        try:
+            model.load_state_dict(torch.load(self.model_path))
+        except Exception as e:
+            TypeError("model_path is not a valid torchvision model path: ", e)
+
+        self.set_model(model)
+
+    def set_model(self, model: Any):
+        """
+        Sets the underlying TorchVision model.
+        Args:
+            model: Any
+                A TorchVision model
+        """
+        check_requirements(["torch", "torchvision"])
+
+        model.eval()
+        self.model = model.to(self.device)
+
+        # set category_mapping
+        from sahi.utils.torchvision import COCO_CLASSES
+
+        if self.category_mapping is None:
+            category_names = {str(i): COCO_CLASSES[i] for i in range(len(COCO_CLASSES))}
+            self.category_mapping = category_names
+
+    def perform_inference(self, images: List):
+        """
+        Prediction is performed using self.model and the prediction result is set to self._original_predictions.
+        Args:
+            images: List[np.ndarray, PIL.Image.Image]
+                A numpy array that contains a list of images to be predicted. 3 channel image should be in RGB order.
+        """
+        from sahi.utils.torch import to_float_tensors
+
+        if not isinstance(images, list):
+            images = [images]
+
+        images = to_float_tensors(images, device=self.device)
+
+        # arrange model input size
+        if self.image_size is not None:
+            # get min and max of image height and width
+            min_shape, max_shape = min(images[0].shape[1:]), max(images[0].shape[1:])
+            # torchvision resize transform scales the shorter dimension to the target size
+            # we want to scale the longer dimension to the target size
+            image_size = self.image_size * min_shape / max_shape
+            self.model.transform.min_size = (image_size,)  # default is (800,)
+            self.model.transform.max_size = image_size  # default is 1333
+
+        prediction_result = self.model(images)
+
+        self._original_predictions = prediction_result
+
+    @property
+    def num_categories(self):
+        """
+        Returns number of categories
+        """
+        return len(self.category_mapping)
+
+    @property
+    def has_mask(self):
+        """
+        Returns if model output contains segmentation mask
+        """
+        return self.model.with_mask
+
+    @property
+    def category_names(self):
+        return list(self.category_mapping.values())
+
+    def _create_object_predictions_from_original_predictions(
+        self,
+        offset_amounts: Optional[List[List[int]]] = [[0, 0]],
+        full_shapes: Optional[List[List[int]]] = None,
+    ):
+        """
+        self._original_predictions is converted to a list of prediction.ObjectPrediction and set to
+        self._object_predictions_per_image.
+        Args:
+            offset_amounts: list of list
+                To remap the box and mask predictions from sliced image to full sized image, should
+                be in the form of List[[offset_x, offset_y],[offset_x, offset_y],...]
+            full_shapes: list of list
+                Size of the full image after remapping, should be in the form of
+                List[[height, width],[height, width],...]
+        """
+        original_predictions = self._original_predictions
+
+        for image_ind, image_predictions in enumerate(original_predictions):
+            object_predictions_per_image = []
+
+            # get indices of boxes with score > confidence_threshold
+            scores = image_predictions["scores"].cpu().detach().numpy()
+            selected_indices = np.where(scores > self.confidence_threshold)[0]
+
+            # parse boxes, masks, scores, category_ids from predictions
+            category_ids = image_predictions["labels"][selected_indices].tolist()
+            boxes = image_predictions["boxes"][selected_indices].tolist()
+            scores = scores[selected_indices]
+
+            # check if predictions contain mask
+            masks = image_predictions.get("masks", None)
+            if masks is not None:
+                masks = image_predictions["masks"][selected_indices].tolist()
+            else:
+                masks = None
+
+            # create object_predictions
+            object_predictions = []
+
+            offset_amount = offset_amounts[image_ind]
+            full_shape = None if full_shapes is None else full_shapes[image_ind]
+
+            for ind in range(len(boxes)):
+                if masks is not None:
+                    mask = np.array(masks[ind])
+                else:
+                    mask = None
+
+                object_prediction = ObjectPrediction(
+                    bbox=boxes[ind],
+                    bool_mask=mask,
+                    category_id=int(category_ids[ind]),
+                    category_name=self.category_mapping[str(int(category_ids[ind]))],
+                    offset_amount=offset_amount,
+                    score=scores[ind],
+                    full_shape=full_shape,
+                )
+                object_predictions.append(object_prediction)
+            object_predictions_per_image.append(object_predictions)
+
+        self._object_predictions_per_image = object_predictions_per_image
