6d9ff266be83a42fe13976c165ed17557e5f3543
Onuralp SEZER <thunderbirdtr@gmail.com>
2025-10-13 23:23:42 +0300

    fix: 🐛 correct error message for invalid model path in RTDetrDetectionModel (#1266)
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- sahi/models/rtdetr.py
+++ sahi/models/rtdetr.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from sahi.models.ultralytics import UltralyticsDetectionModel
 
+
 class RTDetrDetectionModel(UltralyticsDetectionModel):
     def load_model(self):
         """Detection model is initialized and set to self.model."""
@@ -13,5 +14,4 @@ class RTDetrDetectionModel(UltralyticsDetectionModel):
             model.to(self.device)
             self.set_model(model)
         except Exception as e:
-            raise TypeError("model_path is not a valid rtdet model path: ", e)
-
+            raise TypeError("model_path is not a valid rtdetr model path: ", e)
