047d4b087d82de2496bc09334319c6b35dbaa93b
Onuralp SEZER <thunderbirdtr@gmail.com>
2025-10-13 23:43:49 +0300

    feat: ✨ add YOLOWorldDetectionModel and update model type mapping
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- sahi/models/yolo-world.py
+++ sahi/models/yolo-world.py
@@ -3,15 +3,16 @@ from __future__ import annotations
 from sahi.models.ultralytics import UltralyticsDetectionModel
 
 
-class RTDetrDetectionModel(UltralyticsDetectionModel):
+class YOLOWorldDetectionModel(UltralyticsDetectionModel):
     def load_model(self):
         """Detection model is initialized and set to self.model."""
-        from ultralytics import RTDETR
+
+        from ultralytics import YOLOWorld
 
         try:
-            model_source = self.model_path or "rtdetr-l.pt"
-            model = RTDETR(model_source)
+            model_source = self.model_path or "yolov8s-worldv2.pt"
+            model = YOLOWorld(model_source)
             model.to(self.device)
             self.set_model(model)
         except Exception as e:
-            raise TypeError("model_path is not a valid rtdet model path: ", e)
+            raise TypeError("model_path is not a valid yolo world model path: ", e)
