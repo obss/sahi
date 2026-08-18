152672371afece6b2cd070b2446b3a3b4b531797
IIIllllIlIlllII <brian.dasilvapontes@gmail.com>
2026-08-18 08:13:50 +0100

    fix: apply project formatting

--- tests/test_libreyolo_model.py
+++ tests/test_libreyolo_model.py
@@ -10,9 +10,7 @@ from sahi.prediction import ObjectPrediction
 from sahi.utils.cv import read_image
 from sahi.utils.libreyolo import LibreYoloTestConstants, download_libreyolo9t_model
 
-pytestmark = pytest.mark.skipif(
-    importlib.util.find_spec("libreyolo") is None, reason="libreyolo is not installed"
-)
+pytestmark = pytest.mark.skipif(importlib.util.find_spec("libreyolo") is None, reason="libreyolo is not installed")
 
 MODEL_DEVICE = "cpu"
 CONFIDENCE_THRESHOLD = 0.3
